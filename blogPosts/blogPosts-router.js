beb56c662a33ca71d99358412c339da015d238f0
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-11 16:17:22 -0600

    added new post

--- blogPosts/blogPosts-router.js
+++ blogPosts/blogPosts-router.js
@@ -19,8 +19,32 @@ router.get('/', (req, res) => {
 });
 
 // get post by ID
+// NEED TO CORRECT logic if post not found, currently returns 200 and empty array
 router.get('/:id', (req, res) => {
-  
-})
+  blogPosts.findById(req.params.id)
+    .then(post => {
+      {post ? res.status(200).json(post) : res.status(404).json({ message: 'Post not found in blogPosts-router.js, get post by ID' })}
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'Error getting post by ID in blogPosts-router.js '});
+    })
+});
+
+// post new blog post
+router.post('/', (req, res) => {
+  const { title, contents } = req.body;
+  if( !title || !contents ) {
+    res.status(400).json({ message: 'Please provide a title and content for this post!' })
+  } else {
+    blogPosts.insert(req.body)
+      .then(post => {
+        res.status(201).json(post)
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: 'Error making a new post in blogPosts-router.js, post to / ', error})
+      })
+  }
+});
 
 module.exports = router;
